76bc0da3879682b9e9a073c3c92a036b2c7368ef
k248336 <k248336@nu.edu.pk>
2026-04-10 20:59:42 +0500

    feat:upload image

--- rihaish/urls.py
+++ rihaish/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,3 +16,9 @@ urlpatterns = [
     path('api/v1/', include('apps.upload.urls')),
     path('api/v1/', include('apps.settings_app.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # This allows serving media files in production/Render if no other storage is used
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
