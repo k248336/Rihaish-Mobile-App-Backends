6328fec9baf22f159922476542163111b904883f
k248336 <k248336@nu.edu.pk>
2026-04-15 18:34:18 +0500

    feat:image upload fixed

--- rihaish/urls.py
+++ rihaish/urls.py
@@ -3,16 +3,10 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+# from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # API Schema and Documentation
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    # Optional UI:
-    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
     # API v1 routes
     path('api/v1/auth/', include('apps.authentication.urls')),
